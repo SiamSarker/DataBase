14ca9787b364e97f38338a77e432f6f0f0e5dfae
SiamSarker <sarkersiam2@gmail.com>
2021-04-18 11:07:54 +0600

    sql

--- 211 - DBMS/subquery_class.sql
+++ 211 - DBMS/subquery_class.sql
@@ -115,8 +115,72 @@ FROM (
 
 
 
--- 3. show all those
---
+-- 3. for each year find out the minimum no of employees
+-- hired by any department.
+
+
+SELECT MIN(dt.num_emp)
+FROM
+(
+    SELECT YEAR(hire_date) as year, department_id as d_id,
+    COUNT(*) as num_emp
+    FROM employees  
+    GROUP BY YEAR(hire_date), department_id
+) AS dt
+GROUP BY dt.year;
+
+
+
+
+-- 4. for each employee,
+-- show his employee id,
+-- total no of employee hired after him and
+-- total no of employee hired before him
+
+
+SELECT myself.employee_id as my,
+    COUNT(*) as after_num_emp
+FROM employees AS myself
+    JOIN 
+    employees AS col
+    ON myself.hire_date < col.hire_date
+GROUP BY myself.employee_id;
+
+
+SELECT myself1.employee_id as my1,
+    COUNT(*) as before_num_emp
+FROM employees AS myself1
+    JOIN 
+    employees AS col1
+    ON myself1.hire_date > col1.hire_date
+GROUP BY myself1.employee_id;
+
+
+-- final query
+
+SELECT af_tb.my,
+    af_tb.after_num_emp,
+    bf_tb.before_num_emp
+FROM (
+    SELECT myself.employee_id as my,
+    COUNT(*) as after_num_emp
+FROM employees AS myself
+    JOIN 
+    employees AS col
+    ON myself.hire_date < col.hire_date
+GROUP BY myself.employee_id
+) AS af_tb
+JOIN 
+(
+    SELECT myself1.employee_id as my1,
+    COUNT(*) as before_num_emp
+FROM employees AS myself1
+    JOIN 
+    employees AS col1
+    ON myself1.hire_date > col1.hire_date
+    GROUP BY myself1.employee_id
+) AS bf_tb
+ON af_tb.my = bf_tb.my1;
 
 
 
@@ -125,8 +189,38 @@ FROM (
 
 
 
+-- Corelated subquery
 
 
+-- find out the highest salary employee details 
+-- from each department.
+
+-- way - 1: row subquery
+
+-- department wise max salary
+
+
+
+
+SELECT *
+FROM employees
+WHERE (department_id, salary) = ANY (
+    SELECT department_id, MAX(salary) as max_sal
+    FROM employees
+    GROUP BY department_id
+);
+
+
+
+-- corelated subquery
+
+SELECT *
+FROM employees e1
+WHERE salary = (
+    SELECT MAX(salary)
+    FROM employees as e2
+    WHERE e2.department_id = e1.department_id
+);
 
 
 
