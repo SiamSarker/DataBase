993ac21a22a91e868cfbed6c45130bbb8b7952cb
SiamSarker <sarkersiam2@gmail.com>
2021-04-19 23:42:57 +0600

    hackerrank

---
+++ Hackerrank/Ollivander's Inventory.sql
@@ -0,0 +1,14 @@
+SELECT id, p.age, w.coins_needed, w.power
+FROM 
+    (
+        SELECT code, power, MIN(coins_needed) as mini
+        FROM Wands
+        GROUP BY code
+    ) as m
+    JOIN
+    Wands as w on w.code = m.code AND w.power = m.power AND w.coins_needed = m.mini
+    JOIN
+    Wands_Property as p on p.code = w.code
+WHERE p.is_evil = 0
+ORDER BY w.power DESC, p.age DESC;
+    
