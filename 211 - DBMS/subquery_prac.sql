0a54876fb9ee622f763164f415db0d6d8e0c11b4
SiamSarker <sarkersiam2@gmail.com>
2021-04-27 03:25:37 +0600

    subquery practice done

--- 211 - DBMS/subquery_prac.sql
+++ 211 - DBMS/subquery_prac.sql
@@ -131,6 +131,110 @@ WHERE salary = (
 );
 
 
+/* practice 6: Show the department wise most senior employees 
+experience value in no of days. */
+
+SELECT DATEDIFF(NOW(), em1.hire_date) 
+FROM employees as em1
+WHERE hire_date = (
+    SELECT MAX(hire_date)
+    FROM employees as em2
+    WHERE em2.department_id = em1.department_id
+);
+
+/* practice 6.1: Show the department wise most senior employee details. */
+SELECT em1.* 
+FROM employees as em1
+WHERE hire_date = (
+    SELECT MAX(hire_date)
+    FROM employees as em2
+    WHERE em2.department_id = em1.department_id
+);
+
+
+/* Type 4 >> Show the department wise nth highest salary value */
+SELECT DEPARTMENT_ID, SALARY FROM employees as emp1
+WHERE 5-1 = (
+    SELECT COUNT(DISTINCT SALARY)
+    FROM employees as emp2
+    WHERE emp2.SALARY>emp1.SALARY
+    AND emp1.DEPARTMENT_ID=emp2.DEPARTMENT_ID
+);
+/* for 4th highest salary value, use n-1=4-1=3 */ /* for 2nd highest salary value, use n-1=2-1=1 */
+/* Type 4.1 >> Show the department wise nth highest salary holder employee details */
+SELECT *
+FROM employees as emp1 WHERE 5-1 = (
+    SELECT COUNT(DISTINCT SALARY)
+    FROM employees as emp2
+    WHERE emp2.SALARY>emp1.SALARY
+    AND emp1.DEPARTMENT_ID=emp2.DEPARTMENT_ID
+);
+
+/* practice 7: Show department wise 2nd lowest salary value */
+/* practice 7.1: Show department wise 2nd lowest salary holder employee details */
+
+
+/* Type 5 >> Show the manager id(s) managing maximum no of employees */
+SELECT man.employee_id, COUNT(man.employee_id) AS maxcnt 
+FROM employees man
+GROUP BY man.manager_id
+HAVING COUNT(man.employee_id) = (
+    SELECT MAX(man1.total) 
+    FROM (
+    SELECT COUNT(*) AS total FROM employees e1
+    GROUP BY e1.manager_id
+    ) AS man1 
+);
+
+
+/* practice 9: show the manager details managing minimum no of employees */
+SELECT man.*
+FROM employees man
+GROUP BY man.manager_id
+HAVING COUNT(man.employee_id) = (
+    SELECT MIN(man1.total) 
+    FROM (
+    SELECT COUNT(*) AS total FROM employees e1
+    GROUP BY e1.manager_id
+    ) AS man1 
+);
+
+
+/* practice 10: show the department details having maximum no of employees */
+
+SELECT d.*
+FROM employees d
+GROUP BY d.department_id
+HAVING COUNT(d.employee_id) = (
+    SELECT MAX(d1.total) 
+    FROM (
+    SELECT COUNT(*) AS total FROM employees e1
+    GROUP BY e1.department_id
+    ) AS d1 
+);
+
+
+
+
+/* Type 5.1 >> Show the manager managing nth highest total no of employees */
+SELECT mantable.man_id, mantable.emp_cnt 
+FROM
+(
+    SELECT e.manager_id AS man_id, COUNT(e.employee_id) AS emp_cnt FROM employees e
+    GROUP BY e.manager_id
+) AS mantable
+JOIN
+(
+    SELECT e.manager_id AS man_id, COUNT(e.employee_id) AS emp_cnt FROM employees e
+    GROUP BY e.manager_id
+) AS mantable1
+ON mantable.emp_cnt < mantable1.emp_cnt
+GROUP BY mantable.man_id
+HAVING COUNT(DISTINCT mantable1.emp_cnt) = 5-1;
+/* for 4th highest salary value, use n-1=4-1=3 */ 
+/* for 2nd highest salary value, use n-1=2-1=1 */
+
+
 
 
 
